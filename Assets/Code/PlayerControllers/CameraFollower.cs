49096b7641cc809e0b5b5e8fbb98bb91a568517c
maxim <maksimya@bk.ru>
2022-01-11 16:27:14 +0300

    CameraFixes

--- Assets/Code/PlayerControllers/CameraFollower.cs
+++ Assets/Code/PlayerControllers/CameraFollower.cs
@@ -1,36 +1,86 @@
-﻿using UnityEngine;
+﻿using System;
+using DG.Tweening;
+using UnityEngine;
+using UnityEngine.Serialization;
 
 namespace Code.PlayerControllers
 {
     public class CameraFollower : MonoBehaviour
     {
-        [SerializeField] private Vector3 offset;
-        [SerializeField] private Transform target;
+        [SerializeField] private Vector3 shoulderOffset;
+        [SerializeField] private Vector3 cameraOffset;
+        
+        [SerializeField] private Transform followTarget;
+        [SerializeField] private Transform cameraTransform;
+
+        [SerializeField] private Transform requiredPoint;
+        [SerializeField] private Transform cameraPoint;
+        [SerializeField] private LayerMask cameraBordersMask;
+        [SerializeField] private float maxDistance;
 
         [Range(0f, 10f)]
         [SerializeField] private float smooth = 0.1f;
-        
-        [SerializeField] private float rotationSmooth = 50f;
 
+        private void Awake()
+        {
+            maxDistance = Vector3.Distance(transform.position, cameraPoint.position);
+        }
 
         private void Update()
         {
-            if(target == null)
+            if(followTarget == null)
                 return;
+
+            transform.position = followTarget.position + shoulderOffset;
             
-            transform.position = Vector3.Lerp(transform.position, target.position + offset, smooth * Time.deltaTime);
-            
-            var targetRotation = target.eulerAngles.y;
-            Debug.Log(targetRotation);
-            transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(0f,targetRotation,0f), rotationSmooth * Time.deltaTime);
+            cameraTransform.position = Vector3.Lerp(cameraTransform.position, cameraPoint.position, smooth * Time.deltaTime);
+            cameraTransform.rotation = Quaternion.Lerp(cameraTransform.rotation, cameraPoint.rotation, smooth * Time.deltaTime);
         }
 
+        public void RotateCamera()
+        {
+            //fit shoulder rotation
+            var rotation = followTarget.eulerAngles;
+            rotation.x = transform.eulerAngles.x;
+            rotation.z = transform.eulerAngles.z;
+            transform.eulerAngles = rotation;
+
+
+            //var requiredPosition = followTarget.position + shoulderOffset + cameraOffset;
+            
+            var targetPosition = followTarget.position + shoulderOffset;
+            var direction = (requiredPoint.position -  targetPosition).normalized;
+            var ray = new Ray(targetPosition ,direction);
+            
+            Debug.DrawRay(ray.origin,ray.direction, Color.magenta,10f);
+
+            var hasCollision = Physics.SphereCast(ray, 0.1f, out var hit, maxDistance, cameraBordersMask);
+            cameraPoint.position = hasCollision ? hit.point : ray.GetPoint(maxDistance);
+            
+            //FitCamera(true);
+        }
+
+        private void FitCamera(bool immediate = false)
+        {
+            if (immediate)
+            {
+                cameraTransform.position = cameraPoint.position;
+                cameraTransform.rotation = cameraPoint.rotation;
+                return;
+            }
+            
+            //TODO: dotween move
+        }
+        
         private void OnValidate()
         {
-            if(target == null)
+            if(followTarget == null || cameraPoint == null || cameraTransform == null)
                 return;
 
-            transform.position = target.position + offset;
+            transform.position = followTarget.position + shoulderOffset;
+            cameraPoint.localPosition = cameraOffset;
+
+            FitCamera(true);
         }
     }
 }
